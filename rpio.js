0682cc5d8e330b2730f8187b27f7b9f96ea33707
Adminy <adminy@users.noreply.github.com>
2023-05-20 17:48:37 +0100

    Update rpio.js

--- rpio.js
+++ rpio.js
@@ -3,8 +3,10 @@ import rpio from 'rpio'
 import App from 'fastify'
 const gpioPins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 27, 28, 29, 31, 32, 33, 35, 36, 37, 38, 40]
 rpio.init({ gpiomem: true, mapping: 'physical', close_on_exit: true }) // pins 1-40, 28 lights
+const states = {}
 for (const pin of gpioPins) {
    rpio.open(pin, rpio.OUTPUT, rpio.LOW) // rpio.HIGH
+   states[pin] = false
 }
 
 const app = App()
@@ -12,6 +14,17 @@ const app = App()
 app.get('/:pin/:on', req => {
    const pin = parseInt(req.params.pin)
    const on = parseInt(req.params.on)
+   if (gpioPins.includes(pin)) {
+      rpio.write(pin, on ? rpio.HIGH : rpio.LOW)
+      states[pin] = on
+   }
+   return 'ok'
+})
+
+app.get('/:pin', req => {
+   const pin = parseInt(req.params.pin)
+   const on = !states[pin]
+   states[pin] = on
    gpioPins.includes(pin) && rpio.write(pin, on ? rpio.HIGH : rpio.LOW)
    return 'ok'
 })
