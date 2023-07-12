4032467dd09ae4044f6161f26d5cb31c0ce55953
adminy <marinbivol10@gmail.com>
2023-07-12 16:25:17 +0000

    fix platform issues

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,11 +1,12 @@
-const cwd = '/root/home'
+const cwd = '/root/home' 
+const pins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 27, 28, 29, 31, 32, 33, 35, 36, 37, 38, 40]
 module.exports = {
-  apps : [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 27, 28, 29, 31, 32, 33, 35, 36, 37, 38, 40].map(pin => ({
+  apps : pins.map(pin => ({
 	name: 'app' + pin,
-	script: '.',
+	script: 'index.cjs',
 	args: '' + pin,
 	cwd
-  })).concat([{ name: 'rpio', script: 'rpio', cwd }])
+  })).concat([{ name: 'rpio', script: 'rpio.js', cwd }])
 }
 
 
